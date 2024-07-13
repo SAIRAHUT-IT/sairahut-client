b4a63a31b0dd7868b9bed4f91894ab65b7fa0164
phufaphu <pphufaphu@gmail.com>
2024-07-13 23:09:46 +0700

    update: hint layout & button modal comp 👽

---
+++ src/lib/components/HintButton.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+	import { ShieldHalf } from 'lucide-svelte';
+    export let isUnlocked = false;
+</script>
+
+<div class="mx-5">
+	<label class="block text-lg font-bold mb-1 ml-2" for="field1">คำใบ้ 1</label>
+	<Dialog.Root>
+		<Dialog.Trigger class="w-full relative" disabled={isUnlocked}>
+            {#if !isUnlocked}
+                <div class="absolute flex items-center justify-center bg-gray-200 w-full h-full rounded-2xl">
+                    <ShieldHalf class="text-2xl text-white/95 drop-shadow-[0_3px_3px_rgba(0,0,0,0.25)]" />
+                </div>
+            {/if}
+			<input
+				class="border shadow rounded-2xl w-full h-12 py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline pointer-events-none"
+				id="field1"
+				type="text"
+				placeholder="Lorem ipsum"
+				readonly
+			/>
+		</Dialog.Trigger>
+		<Dialog.Content class="sm:max-w-[425px]">
+			<Dialog.Header>
+				<Dialog.Title class="text-start text-xl font-semibold">abc</Dialog.Title>
+			</Dialog.Header>
+			<Input id="name" value="" placeholder="Provide Code Here" class="col-span-1" />
+			<Dialog.Footer>
+				<Button type="submit">Submit</Button>
+			</Dialog.Footer>
+		</Dialog.Content>
+	</Dialog.Root>
+</div>
---
+++ src/routes/hint/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	// import type { PageData } from './$types';
+	import { ArrowLeftFromLine, ShieldHalf } from 'lucide-svelte';
+	import HintButton from '$lib/components/HintButton.svelte';
+	let point = 0;
+	// export let data: PageData;
+	let hint = [
+		{
+			question: 'Hint 1: What is the name of your father?'
+		}
+	];
+</script>
+
+<div class="h-dvh mt-4 overflow-y-scroll">
+	<div class="space-y-5">
+		<div id="game-header" class="flex items-center justify-between mx-5 h-12">
+			<div class="flex items-center justify-center gap-2">
+				<a href="/">
+					<ArrowLeftFromLine size={32} />
+				</a>
+				<p class="text-2xl font-bold text-start">Phase 3: Topic</p>
+			</div>
+		</div>
+		<div id="description" class="border shadow rounded-2xl mx-5 px-4 py-2 text-lg">Description</div>
+		<div
+			id="score"
+			class="flex justify-center items-center bg-gray-600 rounded-2xl px-4 py-3 mx-5 text-white text-xl font-bold"
+		>
+			<span class="pr-2">Your point :</span>
+			<p class="text-4xl">{point} pt</p>
+		</div>
+        <HintButton isUnlocked={false}/>
+    </div>
+</div>

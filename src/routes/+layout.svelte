b20f445d176eed1cfcdac1c58d8238448474cba3
Bss-bass <sivapat13@gmail.com>
2024-07-18 17:03:23 +0700

    update(ui): cleaned ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,10 @@
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 </script>
-<!-- <div class="block sm:hidden"> -->
-	<header>
-		<Navbar/>
-	</header>
-	<main class="relative min-h-screen">
-		<slot />
-	</main>
-<!-- </div> -->
+
+<header>
+	<Navbar />
+</header>
+<main class="relative min-h-screen z-10 overflow-hidden">
+	<slot />
+</main>

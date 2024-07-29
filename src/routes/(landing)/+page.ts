c4c1d71a6fbf06a829d4d6a44fdc7facf4137cb7
Zensos <rockemaner@gmail.com>
2024-07-29 21:36:29 +0700

    hotfix: url response

--- src/routes/(landing)/+page.ts
+++ src/routes/(landing)/+page.ts
@@ -1,7 +1,9 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const response = await fetch('/api/auth/signin').then((res) => res.text());
+	const response = await fetch('/api/auth/signin')
+		.then((res) => res.json())
+		.then((res) => res.data);
 	return {
 		url: response
 	};

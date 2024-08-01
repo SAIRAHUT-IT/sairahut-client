f97fe28634e8b0532fb48329a035085a4fd6d08d
Zensos <rockemaner@gmail.com>
2024-08-01 11:57:38 +0700

    update: nav text

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,15 +4,16 @@
 </script>
 
 <nav
-	class="sticky top-0 flex w-full justify-between items-center bg-gradient-to-r from-black to-yellow-900 px-4 z-[1500]"
+	class="sticky top-0 flex w-full justify-between items-center bg-gradient-to-r from-black to-yellow-900 px-4 py-2 z-[1500]"
 >
 	<button
 		on:click={() => {
 			window.location.replace('/menu');
 		}}
-		class="flex items-center"
+		class="flex items-center mangorn text-white text-4xl drop-shadow-[0_1px_3px_#FFFFFF]"
 	>
-		<img src="/logo.svg" alt="logo" class="object-cover w-28" />
+		<!-- <img src="/logo.svg" alt="logo" class="object-cover w-28" /> -->
+		Sairahut IT22
 	</button>
 
 	<button

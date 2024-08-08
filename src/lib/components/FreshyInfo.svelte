a672a9cb3652abd61add5dcc5f3372fa46738535
Zensos <rockemaner@gmail.com>
2024-08-08 11:10:21 +0700

    hotfix: http parser

--- src/lib/components/FreshyInfo.svelte
+++ src/lib/components/FreshyInfo.svelte
@@ -16,7 +16,7 @@
 			on:click={() => window.open(removeSearchQuery(hint.content), '_blank')}
 			class="rounded-xl flex justify-center flex-wrap px-3 py-3 w-full text-slate-50 bg-gradient-to-tr from-[#1D1A17] to-[#302c1cb4] pointer-events-none shadow-md border-r-2 border-l-2 border-[#C99949] max-h-72"
 		>
-			{removeSearchQuery(hint.content) || '??'}
+			{hint.content.includes('http') ? removeSearchQuery(hint.content) : hint.content || '??'}
 		</button>
 	{:else}
 		<div

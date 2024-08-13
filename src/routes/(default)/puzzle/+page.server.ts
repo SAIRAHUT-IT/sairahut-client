3740f3baf98b088f1977dd1d0888308da9c4858e
Zensos <rockemaner@gmail.com>
2024-08-13 13:09:25 +0700

    update: protect iamge

--- src/routes/(default)/puzzle/+page.server.ts
+++ src/routes/(default)/puzzle/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageLoad } from '../../(landing)/$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-	const image = await fetch('/api/puzzle/image').then((res) => res.text());
+	const { image } = await fetch('/api/puzzle/image').then((res) => res.json());
 	return {
 		image
 	};
